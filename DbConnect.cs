e8e455946f7f7ba6a2c56470cca6236aaec8a1c7
Mugiwara-boi <111000560+Mugiwara-boi@users.noreply.github.com>
2023-07-27 18:20:58 +0800

    read table

---
+++ DbConnect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Kape
+{
+    public class DbConnect
+    {
+        private MySqlConnection connection;
+        private string connectionString;
+
+        public DbConnect()
+        {
+            connectionString = "server=localhost;user=root;database=db_inventory_system;";
+            connection = new MySqlConnection(connectionString);
+
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+        }s
+        public DataTable GetComputerPartsData()
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                OpenConnection();
+
+                string query = "SELECT ID, Name, Count, Category, Manufacturer, Specifications, Price FROM computer_parts;";
+                MySqlCommand cmd = new MySqlCommand(query, GetConnection());
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception appropriately, e.g., show an error message.
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return dataTable;
+        }
+    }
+}
--- Inventory.cs
+++ Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kape;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace Group8Sytem
 {
     public partial class Inventory : UserControl
     {
+        // Define the public DataGridView control
+       // public DataGridView dataGridInventory;
+
         public Inventory()
         {
             InitializeComponent();
+            PopulateDataGrid(); // populate the dataGridInventory
+        }
+
+        public void PopulateDataGrid()
+        {
+            try
+            {
+                DbConnect dbConnector = new DbConnect();
+                DataTable dataTable = dbConnector.GetComputerPartsData();
+
+                dataGridInventory.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception appropriately, e.g., show an error message.
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Public method to get the dataGridInventory control
+        public DataGridView GetDataGridInventory()
+        {
+            return dataGridInventory;
         }
     }
 }
